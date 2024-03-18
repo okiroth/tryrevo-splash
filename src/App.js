d145775de87db9decb6e9684486853176a000724
Ivan Roth <2123178+okiroth@users.noreply.github.com>
2024-03-18 17:48:00 -0300

    update icons and fonts

--- src/App.js
+++ src/App.js
@@ -25,37 +25,37 @@ function App() {
     {
       icon: <Icon1 />,
       iconSmall: <Icon1Small />,
-      text: "Explore all EV makes and models. Compare them side-by side.",
+      text: "Explore all EV makes and models. Compare them side-by side",
     },
     {
       icon: <Icon2 />,
       iconSmall: <Icon2Small />,
-      text: "Calculate your range and build your charge plan.",
+      text: "Calculate your range and build your charge plan",
     },
     {
       icon: <Icon3 />,
       iconSmall: <Icon3Small />,
-      text: "Find the best deal with exclusive offers from Revo’s dealer network.",
+      text: "Find the best deal with exclusive offers from Revo’s dealer network",
     },
     {
       icon: <Icon4 />,
       iconSmall: <Icon4Small />,
-      text: "Lean on our advisors to guide you along the way.",
+      text: "Lean on our advisors to guide you along the way",
     },
   ];
 
   return (
     <div className="flex flex-col items-stretch min-h-screen">
       <div className="p-2 text-big top-img flex flex-col justify-center items-center text-center">
+        <div>Find your next electric vehicle</div>
         <div>
-          Find your next <strong>EV</strong>
+          with <strong>rEVo</strong>
         </div>
-        <div>with Revo</div>
       </div>
       <div className="grow top-wrap">
         <div className="md:container md:mx-auto flex flex-col content-center place-items-center lg:gap-20 gap-5 lg:pt-20 pt-5 text-center">
           <div className="text-big p-2">
-            We are along for <br /> the ride with you.
+            We're along for <br /> the ride with you.
           </div>
           <div className="lg:flex text-small">
             {Icons.map((icon) => (
@@ -76,7 +76,7 @@ function App() {
           <div className="divider" />
         </div>
         <div className="text-white flex flex-col content-center place-items-center p-20 text-center">
-          <div className="text-md">
+          <div className="text-big">
             Sign up for early <br /> access today
           </div>
           <div className="p-10" style={{ display: sent ? "block" : "none" }}>
@@ -86,7 +86,7 @@ function App() {
             onSubmit={handleSubmit}
             method="POST"
             action="https://script.google.com/macros/s/AKfycbyJ0m3n9GEbngkbLP1NZYs4DWvNHRMWUbVynJavGmRGvNS7Ys8aNfJWe_E20Ft1frXmZw/exec"
-            className="flex flex-col content-center place-items-center gap-5 mt-5"
+            className="flex flex-col content-center place-items-center gap-5 mt-20"
             style={{ display: sent ? "none" : "flex" }}
           >
             <input
